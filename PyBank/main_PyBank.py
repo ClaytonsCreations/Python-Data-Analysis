21073fe3e61b7e86b7f0723cb2605413738556ff
clytn579 <clytn579@gmail.com>
2021-09-28 15:15:08 -0700

    update after TA help

--- PyBank/main_PyBank.py
+++ PyBank/main_PyBank.py
@@ -10,28 +10,20 @@ profit_loss = []
 change = []
 
 #variables
-months = 0
+month = 0
 profit_loss = 0
 
 
-with open('pybank', newline="") as csvfile:
+with open(pybank) as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csv_reader)
    
     #The total number of months included in the dataset
     for row in csv_reader: 
-       temp.append(postag(row))
+       months.append(row[0])
+       print(months)
 
-    for i in range(1, len(profit_loss)):
-        change.append((int(profit_loss[i]) - int(profit_loss[i-1])))
-
-    print(f"Financial Analysis")
-    print("-" * 26)
-    print(f"Total Months: {len(list(months))}")
-    print(f"Total: ${sum(profit_loss)}")
-    print(f"Average Change: ${round(sum(change) / len(change), 2)}")
-    print(f"Greatest Increase in Profits: {months[25]} (${max(change)})")
-    print(f"Greatest Decrease in Profits: {months[44]} (${min(change)})")
+  
 
 
     #The net total amount of "Profit/Losses" over the entire period
